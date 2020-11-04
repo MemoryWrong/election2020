38dd4791917992197890ba5fa13a86bb4ebf2389
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-11-04 16:07:38 +0800

    add elect won scores

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import useSWR from 'swr';
-import { Flex, Heading, Spinner } from "@chakra-ui/core"
+import { Flex, Text, Heading, Spinner } from "@chakra-ui/core"
 
 import USAMap from './USAMap';
 import { fetchCandidates, fetchResults } from './api';
 
+function Center({ children }) {
+  return <Flex direction="column" justify="center" align="center" height="100vh">
+    {children}
+  </Flex>
+}
+
 function App() {
   const { data: candidates } = useSWR('candidates', fetchCandidates);
   const { data: results } = useSWR('results', fetchResults, {
-    refreshInterval: 30000,
+    refreshInterval: 60000,
     refreshWhenHidden: true
   });
-
-  // if (!candidates || !results) {
-  //   return 
-  // }
 
   console.log(candidates)
   console.log(results)
 
   const states = {};
+  let trumpElectWon = 0;
+  let bidenElectWon = 0;
+
+  if (!candidates || !results) {
+    return <Center><Spinner size="xl" /></Center>
+  }
 
   if (candidates && results) {
     Object.keys(results).forEach((state) => {
@@ -27,19 +35,23 @@ function App() {
   
       if (winner) {
         states[state] = {
-          fill: candidates[winner.candidateID].fullName === 'Donald Trump' ? '#C93136' : '#1475B7'
+          fill: candidates[winner.candidateID].fullName === 'Donald Trump' ? '#C53030' : '#2B6CB0'
         }
       }
     });
+
+    bidenElectWon = results.US[0].summary.results.find(i => i.candidateID === 'US1036').electWon;
+    trumpElectWon = results.US[0].summary.results.find(i => i.candidateID === 'US8639').electWon;
   }
 
   return (
-    <Flex direction="column" justify="center" align="center" height="100vh">
-      { !candidates || !results ? <Spinner size="xl" /> : <>
-        <Heading as="h2" size="xl" mb={5}>2020 Presidential Election</Heading>
-        <USAMap states={states} />
-      </> }
-    </Flex>
+    <Center>
+      <Heading as="h2" size="xl">2020 Presidential Election</Heading>
+      <Heading as="h3" size="lg" mt={2}>
+        <Text as="span" color="blue.600">{bidenElectWon}</Text> Biden | Trump <span style={{ color: '#C53030' }}>{trumpElectWon}</span>
+      </Heading>
+      <USAMap states={states} />
+    </Center>
   );
 }
 
